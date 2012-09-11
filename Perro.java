7ac2088de44ccad7adc2b0db74569d369a023cba
Ariel Bautista <ariel.bautista1@gmail.com>
2012-09-11 15:46:30 -0400

    first commit

---
+++ Perro.java
@@ -0,0 +1,19 @@
+public class Perro {
+
+    String nombreanimal;
+
+    public Perro()
+    {
+
+      this.nombreanimal = "Perro";  
+
+    }          
+
+    public String decirQuienSoy ()
+   
+    {
+      return this.nombreanimal;
+     
+    }
+
+}
