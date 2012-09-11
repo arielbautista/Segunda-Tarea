b3db0eaa868e8fad4f3dfd4b679f5091b744b2b8
Ariel Bautista <ariel.bautista1@gmail.com>
2012-09-11 15:53:54 -0400

    first commit

---
+++ Gallina.java
@@ -0,0 +1,19 @@
+public class Gallina {
+
+    String nombreanimal;
+
+    public Gallina()
+    {
+
+      this.nombreanimal = "Gallina";  
+
+    }          
+
+    public String decirQuienSoy ()
+   
+    {
+      return this.nombreanimal;
+     
+    }
+
+}
