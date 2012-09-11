efb9bc65bcec155e42a64f53367e02aa38815452
Ariel Bautista <ariel.bautista1@gmail.com>
2012-09-11 16:28:06 -0400

    first commit

---
+++ numero.java
@@ -0,0 +1,12 @@
+public class orden
+     {
+     public static void main(String[] args)
+     {
+     Scanner teclado;
+     teclado = new Scanner (System.in);
+     int variable;
+     variable = teclado.nextInt();
+      for(int i=1; i<variable; i++){}
+        System.out.println("Número: " + teclado);
+      }
+}
