0a3dd51b171fe2fae77a1bd61fbaac137bc8a2b9
Ariel Bautista <ariel.bautista1@gmail.com>
2012-09-11 15:52:07 -0400

    first commit

---
+++ Gato.java
@@ -0,0 +1,19 @@
+public class Gato {
+
+    String nombreanimal;
+
+    public Gato()
+    {
+
+      this.nombreanimal = "Gato";  
+
+    }          
+
+    public String decirQuienSoy ()
+   
+    {
+      return this.nombreanimal;
+     
+    }
+
+}
